7bfa0721a35dbd2deef5b62208f52d7676b4acaf
Y009 <henryj654@gmail.com>
2016-12-19 11:38:49 +0200

    Doxygen update with application sequence diagram

---
+++ LOGIC/application.c
@@ -0,0 +1,127 @@
+/*
+ * application.c
+ *
+ *  Created on: 4. dets 2016
+ *      Author: Y009
+ */
+
+/** \file application.c
+**	\brief Documentation for the application module.
+**
+**  The application module serves as an intermediary between modules to handle work sequence.
+
+	Example of how the application module works:
+
+	@startuml
+	Application -> Timer : Check flag
+	Application -> Button : Debounce
+
+	Application --> Button : Get button
+	Application <-- Button : Return button
+	Application --> Ultrasonic : Get status
+	Application <-- Ultrasonic : Return status
+
+	Application --> Ultrasonic : Send signal
+	Ultrasonic --> usHardware : High signal
+	Ultrasonic --> usHardware : Low signal
+
+	Application -> Ultrasonic : Get status
+	Application <- Ultrasonic : Return status
+	Application --> Ultrasonic : Get data status
+	Application <-- Ultrasonic : Return data status
+
+	Application --> Ultrasonic : Get distance
+	Application <-- Ultrasonic : Return distance
+	Application --> Application : Convert distance to string
+
+	Application --> LCD : Get state
+	Application <-- LCD : Return state
+	Application --> LCD : Send string
+	@enduml
+
+**/
+
+#include "../MCU/timer.h"
+#include "../HW/button.h"
+#include "../HW/ultraS.h"
+#include "../HW/lcd.h"
+#include "../MCU/uart.h"
+#include "../MCU/counter.h"
+
+#define DELAY 10000U							/**< 0.000625 second delay */
+
+void convertUDec(unsigned long n);
+unsigned long int distance;						/**< Local variable of distance to be converted to string */
+
+char String[16] = "Kaugus : ";					/**< String to be displayed on the LCD \b after the distance has been added to it */
+char StringError[16] = "Error!         ";		/**< String to be displayed on the LCD \b if the distance fell out of the measurable range */
+
+void 
+application_cyclic()
+/**	The main cyclic function to control the workflow.
+**
+**	Initiates check on button.
+**	\n If the button has been debounced starts the work (if not already in progress.)
+**	\n Clears the display and checks with ultrasonic module.
+**	\n Sends a string to the LCD module to be displayed based on value from the ultrasonic module.
+**/
+{
+
+	timer_checkFlag();
+	button_debounceBtn();
+
+    if(button_getBtn() && (ultraS_getValidStatus() != US_WORKING)){
+    	ultraS_sendSignal();
+    }
+
+    if (ultraS_getValidStatus() == US_OK) {
+
+    	if (ultraS_getDataStatus() == (US_DATA_FALSEMAX || US_DATA_FALSEMIN || US_DATA_FALSE)){
+
+   		if(lcd_getState() == lcd_IDLE)
+   		{
+    			lcd_sendString(StringError);
+
+   		}
+
+    	}
+    	else {
+    		distance = ultraS_getDistance();            	/* To be actually given to the lcd module */
+            convertUDec(distance);
+
+        	if(lcd_getState() == lcd_IDLE)
+        	{
+        		lcd_sendString(String);
+        	}
+    	}
+
+		ultraS_setDataStatus(US_DATA_READ);
+    }
+}
+
+void
+convertUDec(
+		unsigned long n)						/**< Numeric value to be converted to string. */
+/**	Converts integer value to string
+**
+**	Converts integer to string and places it in \b String to the appropriate position
+**  \n Converts each digit of the value on its own by taking modulo 10 of it and adding a value of 30 to it.
+**	\n Based on the ascii table where numbers are from 30 to 39.
+**
+** \attention Currently limited to 4 digit numbers.
+**/
+{
+	int i;
+    for(i = 12; i >= 9; i--){
+        if((n > 0) || (i == 12))    			/*  Has to have at least 1 digit to have made here or at least something left to convert */
+		String[i] = n % 10 + 0x30;  			/*  Numbers in ascii: 30 - 39 */
+        else                      				/*  Add spaces if no more digits */
+            String[i] = ' ';
+        n = n / 10;                				/*  New digit to use */
+    }
+
+    String[13] = 'c';
+    String[14] = 'm';
+    String[15] = 0;
+}
+
---
+++ MCU/main.c
@@ -0,0 +1,75 @@
+/* --COPYRIGHT--,BSD
+ * Copyright (c) 2016, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --/COPYRIGHT--*/
+
+#include "driverlib.h"
+#include "clock.h"
+#include "uart.h"
+#include "../HW/lcd.h"
+#include "timer.h"
+#include "counter.h"
+#include "../HW/ultraS.h"
+#include "../HW/gpio.h"
+#include "../LOGIC/application.h"
+
+//MISC
+#define DELAY 10000000 //0.625 second delay
+
+
+//******************************************************************************
+//!
+//!   Hardware project: Distance measurer with ultrasonic module.
+//!
+//******************************************************************************
+void main(void)
+{
+	WDT_A_hold(WDT_A_BASE);
+	gpio_init();
+	clkInit();
+	UART_init();
+	timer_init();
+	counter_init();
+	ultraS_init();
+
+	__bis_SR_register(GIE); 						/* Global interrupt enable. */
+
+	if(lcd_getState()==lcd_IDLE)
+	{
+		lcd_sendString(" Hello and bye! ");				/* Welcome message -.-*/
+	}
+
+
+	while(1){
+		application_cyclic();
+        ultraS_cyclic();
+        lcd_cyclic();
+    }
+}
